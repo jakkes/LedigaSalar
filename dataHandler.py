585bee902d33b30bf1bbe69e1e9afa78f7168181
Jakob Stigenberg <jakob@jakke.se>
2017-09-21 22:06:57 +0200

    RC

--- dataHandler.py
+++ dataHandler.py
@@ -1,5 +1,10 @@
 from requests import get
 import json
+import datetime
+import threading
+
+_model = {}
+_rooms = ["A1","A2"]
 
 def _getDepartments():
     try:
@@ -17,7 +22,7 @@ def _getEntries(courseCode):
     try:
         return json.loads(get("https://www.kth.se/api/schema/v2/course/{0}".format(courseCode)).text)
     except json.JSONDecodeError:
-        return []
+        return json.loads('{"entries":[]}')
 
 def _getRooms():
     try:
@@ -25,21 +30,74 @@ def _getRooms():
     except json.JSONDecodeError:
         return []
 
+def init():
+    _setTimer()
+
+def _setTimer():
+    now = datetime.datetime.now()
+    nextRun = datetime.datetime(now.year,now.month,now.day,0,1,0) + datetime.timedelta(days = 1)
+
+    secondsLeft = (nextRun - now).seconds
+
+    print("Setting new model in {0} seconds".format(str(secondsLeft)))
+
+    t = threading.Timer(secondsLeft,_setModel)
+    t.start()
+
 def _setModel():
+
+    print("Setting new model")
+
     deps = _getDepartments()
+    rooms = _getRooms()
+
+    now = datetime.datetime.now()
+
+    model = {}
+    for h in range(now.hour,23):
+        model[h] = [];
+        for room in rooms:
+            model[h].append(room["name"])
+
     for dep in deps:
         courses = _getCourses(dep["code"])
         for course in courses:
-            entries = getEntries(course["code"])
+            entries = (_getEntries(course["code"]))["entries"]
             for entry in entries:
+                start = datetime.datetime.strptime(entry["start"], "%Y-%m-%d %H:%M:%S")
+                end = datetime.datetime.strptime(entry["end"], "%Y-%m-%d %H:%M:%S")
+                
+                if start.date() != now.date() or end.date != now.date():
+                    continue
 
-            print(_getEntries(course["code"]))
+                locations = entry["locations"]
 
-def _getData():
-    deps = _getDepartments()
-    allLocs = []
-    for dep in deps:
-        courses = _getCourses(dep["code"])
-        for course in courses:
-            entries = getEntries(course["code"])
-            for entry in entries:
+                for hour in range(start.hour, end.hour - 1):
+                    for location in locations:
+                        model[hour].remove(location["name"])
+
+    _model = model
+    _rooms = rooms
+
+    print("Model set.")
+    _setTimer()
+
+def FreeRooms(fromHour, toHour):
+    
+    rooms = None;
+    
+    while rooms == None and fromHour < toHour:
+        if fromHour in _model.keys():
+            rooms = _model[fromHour]
+        else:
+            fromHour += 1
+
+    if rooms == None:
+        return _rooms
+
+    for hour in range(fromHour + 1, endHour - 1):
+        for room in rooms:
+            if not room in _model[hour]:
+                rooms.remove(room)
+
+    return rooms
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import urllib.request
 import dataHandler
+from flask import Flask, send_from_directory, request, jsonify
 
-deps = dataHandler._getDepartments()
+app = Flask(__name__)
 
-dataHandler._setModel()
+@app.route("/")
+def index():
+    return send_from_directory("","index.html")
+
+@app.route("/search")
+def search():
+    fromHour = int(request.args.get("from"))
+    toHour = int(request.args.get("to"))
+    return jsonify(dataHandler.FreeRooms(fromHour,toHour))
+
+dataHandler.init()
+
+if __name__=='__main__':
+    app.run()
